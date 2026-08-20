471da649e45b629a70660153edb2570ee5bd3ecd
Mariana Porto <84824402+mariporto13@users.noreply.github.com>
2026-08-20 10:46:53 -0300

    updating code

--- app.js
+++ app.js
@@ -37,10 +37,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     switch (type) {
       case 'Option 1': // AppsFlyer Universal link
-        output += `Landing macro for Android:\n\nServer2server external trackers for Android:\n`;
-        output += `https://app.appsflyer.com/${androidId}?pid=rtbhouse_int&c=${androidC}&af_click_lookback={IMPRESSION_HASH}-{TIMESTAMP}-{CAMPAIGN_HASH}&af_reengagement_window=FALSE&is_retargeting=true&advertising_id={ANDROID_ADVERTISING_ID}&redirect={SSP_ADVERTISER_ENCRYPTED}&clickid={CLIENT_IP_ADDRESS}&af_siteid={RTBHC}&af_ip=\n\n`;
+        output += `Landing macro for Android:\n ${androidRedirect}\n`
+        output += `Server2server external trackers for Android:\n`;
+        output += `https://app.appsflyer.com/${androidId}?pid=rtbhouse_int&c=${androidC}&af_click_lookback=${clWindow}&af_reengagement_window=${reWindow}&is_retargeting=true&advertising_id={ANDROID_ADVERTISING_ID}&redirect=false&clickid={IMPRESSION_HASH}-{TIMESTAMP}-{CAMPAIGN_HASH}&af_siteid={SSP_ADVERTISER_ENCRYPTED}&af_ip={CLIENT_IP_ADDRESS}&rtbhc={RTBHC}\n\n`;
         output += `Landing macro for iOS:\n\nServer2server external trackers for iOS:\n`;
-        output += `https://app.appsflyer.com/${iosId}?pid=rtbhouse_int&c=${iosC}&af_click_lookback={IMPRESSION_HASH}-{TIMESTAMP}-{CAMPAIGN_HASH}&af_reengagement_window=FALSE&is_retargeting=true&idfa={IOS_IDFA}&redirect={SSP_ADVERTISER_ENCRYPTED}&clickid={CLIENT_IP_ADDRESS}&af_siteid={RTBHC}&af_ip=`;
+        output += `https://app.appsflyer.com/${iosId}?pid=rtbhouse_int&c=${iosC}&af_click_lookback=${clWindow}&af_reengagement_window=${reWindow}&is_retargeting=true&idfa={IOS_IDFA}&redirect=false&clickid={IMPRESSION_HASH}-{TIMESTAMP}-{CAMPAIGN_HASH}&af_siteid={SSP_ADVERTISER_ENCRYPTED}&af_ip={CLIENT_IP_ADDRESS}&rtbhc={RTBHC}`;
         break;
 
       case 'Option 2': // AppsFlyer Deeplink
